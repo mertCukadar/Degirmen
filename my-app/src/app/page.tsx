2cf768db73f1c3e40fbb0c30fc38444ef78541c4
mertCukadar <cukadar.mertkaan@gmail.com>
2024-09-23 08:14:08 -0700

    initial commit

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 interface Product {
@@ -7,10 +7,15 @@ interface Product {
   price: string;
 }
 
+interface Subcategory {
+  name: string;
+  products: Product[];
+}
+
 interface MenuTab {
   name: string;
   active: boolean;
-  products: Product[];
+  subcategories?: Subcategory[];
 }
 
 export default function Home() {
@@ -18,40 +23,73 @@ export default function Home() {
     {
       name: "Mayalılar",
       active: false,
-      products: [
-        { name: "Ekmek", price: "15 TL" },
-        { name: "Pide", price: "20 TL" },
-        { name: "Simit", price: "5 TL" },
+      subcategories: [
+        {
+          name: "Ürünler",
+          products: [
+            { name: "Ekmek", price: "15 TL" },
+            { name: "Pide", price: "20 TL" },
+            { name: "Simit", price: "5 TL" },
+          ],
+        },
       ],
     },
     {
       name: "Tatlılar",
       active: false,
-      products: [
-        { name: "Baklava", price: "80 TL" },
-        { name: "Künefe", price: "60 TL" },
-        { name: "Sütlaç", price: "25 TL" },
+      subcategories: [
+        {
+          name: "Ürünler",
+          products: [
+            { name: "Baklava", price: "80 TL" },
+            { name: "Künefe", price: "60 TL" },
+            { name: "Sütlaç", price: "25 TL" },
+          ],
+        },
       ],
     },
     {
       name: "Kurabiyeler",
       active: false,
-      products: [
-        { name: "Un Kurabiyesi", price: "30 TL" },
-        { name: "Cevizli Kurabiye", price: "40 TL" },
-        { name: "Damla Çikolatalı Kurabiye", price: "50 TL" },
+      subcategories: [
+        {
+          name: "Ürünler",
+          products: [
+            { name: "Un Kurabiyesi", price: "30 TL" },
+            { name: "Cevizli Kurabiye", price: "40 TL" },
+            { name: "Damla Çikolatalı Kurabiye", price: "50 TL" },
+          ],
+        },
       ],
     },
     {
       name: "İçecekler",
       active: false,
-      products: [
-        { name: "Çay", price: "10 TL" },
-        { name: "Kahve", price: "20 TL" },
-        { name: "Limonata", price: "15 TL" },
+      subcategories: [
+        {
+          name: "Sıcak İçecekler",
+          products: [
+            { name: "Çay", price: "10 TL" },
+            { name: "Kahve", price: "20 TL" },
+          ],
+        },
+        {
+          name: "Soğuk İçecekler",
+          products: [
+            { name: "Limonata", price: "15 TL" },
+          ],
+        },
       ],
     },
   ]);
+
+  const [isLogoVisible, setIsLogoVisible] = useState(false);
+  const [areTabsVisible, setAreTabsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsLogoVisible(true);
+    setAreTabsVisible(true);
+  }, []);
 
   const handleTabClick = (name: string) => {
     setMenuTabs(
@@ -63,39 +101,47 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
-      
-      <div className="space-y-4 w-full max-w-md">
-      <div className="text-center text-2xl flex items-center justify-center font-bold text-gray-800">
-          <Image src="/logo.jpeg" width={500} height={500} alt="logo" className="rounded-full"/>
+    <div className="min-h-screen flex justify-center items-center bg-white">
+      <div className="space-y-2 w-full max-w-md mb-48">
+        <div
+          className={`text-center text-2xl flex items-center justify-center font-bold text-gray-800 transition-transform duration-5000 ${
+            isLogoVisible ? "translate-y-10 opacity-100" : "translate-y-0 opacity-0"
+          }`}
+        >
+          <div className={`bg-white rounded-full p-6 z-10`}>
+            <Image src="/logo.jpeg" width={250} height={250} alt="logo" className="rounded-full ml-2" />
+          </div>
         </div>
 
-        
-        {menuTabs.map((menuTab) => (
-          <div key={menuTab.name}>
+        {menuTabs.map((menuTab, index) => (
+          <div key={menuTab.name} className="px-12">
             <div
               onClick={() => handleTabClick(menuTab.name)}
-              className={`block p-4 border rounded-lg shadow-md text-center cursor-pointer ${
-                menuTab.active
-                  ? "border-primary text-black bg-white"
-                  : "border-primary text-gray-600 bg-white"
+              className={`block p-4 border-2 shadow-lg text-center cursor-pointer ${
+                menuTab.active ? "border-primary text-black bg-white" : "border-primary text-gray-600 bg-white"
+              } transition-transform duration-5000 ${
+                areTabsVisible ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
               }`}
+              style={{ transitionDelay: `${(index + 1) * 100}ms` }}
             >
               {menuTab.name}
             </div>
             <div
-              className={`overflow-hidden transition-all duration-500 ease-in-out ${
-                menuTab.active ? "max-h-40" : "max-h-0"
-              }`}
+              className={`overflow-hidden transition-all duration-300 ease-in-out ${
+                menuTab.active ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+              } bg-yellow-400 shadow-md rounded-b-2xl`}
             >
-              <div className="p-4 text-gray-700">
-                {menuTab.products.map((product) => (
-                  <div key={product.name} className="flex justify-between">
-                    <span>{product.name}</span>
-                    <span>{product.price}</span>
-                  </div>
-                ))}
-              </div>
+              {menuTab.subcategories?.map((subcategory) => (
+                <div key={subcategory.name} className="p-4 text-gray-700">
+                  <h4 className="font-bold">{subcategory.name}</h4>
+                  {subcategory.products.map((product) => (
+                    <div key={product.name} className="flex justify-between">
+                      <span>{product.name}</span>
+                      <span>{product.price}</span>
+                    </div>
+                  ))}
+                </div>
+              ))}
             </div>
           </div>
         ))}
--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -2,9 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -13,8 +11,26 @@ const config: Config = {
         foreground: "var(--foreground)",
         primary: "#e9af1d",
       },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(-10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1', transform: 'translateY(0)' },
+          '100%': { opacity: '0', transform: 'translateY(-10px)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+        fadeOut: 'fadeOut 0.5s ease-in-out',
+      },
+      transitionDuration: {
+        '5000': '3000ms',
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
